b0ba1e828f26e08de93b19a77a1e93ed8cd3df87
joker-22 <vegesanakarthikvarma@gmail.com>
2019-08-08 23:12:02 +0530

    Add these files

---
+++ TripletsWithSumZero.java
@@ -0,0 +1,10 @@
+class Triplet
+{
+    // Function to check if triplet is present 
+    // arr[]: input array
+    // n: size of the array
+	public boolean findTriplets(int arr[] , int n)
+    {
+        //add code here.
+    }
+}
