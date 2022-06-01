61db9b0bbf3308ccee2dbec61f94573e6383d294
Karthik Varma <karthikvarma@Karthiks-MacBook-Pro.local>
2022-06-01 13:24:55 -0400

    Buildings with an Ocean View

---
+++ Q22.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class Q22 {
+    public static void main(String[] args) {
+
+    }
+
+    public int[] findBuildings(int[] ar) {
+        int n = ar.length;
+        int[] rm = new int[n];
+        ArrayList<Integer> res = new ArrayList<>();
+        int rmax = ar[n - 1];
+        res.add(n - 1);
+        for (int i = n - 2; i >= 0; i--) {
+            if (rmax < ar[i]) {
+                res.add(i);
+                rmax = ar[i];
+            }
+        }
+        int[] out = new int[res.size()];
+        int j = 0;
+        for (int i = res.size() - 1; i >= 0; i--) {
+            out[j++] = res.get(i);
+        }
+        return out;
+    }
+}
