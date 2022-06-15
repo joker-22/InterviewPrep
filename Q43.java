b53e1eade39d72550415a33165916568cb6a4ffc
Karthik Varma <karthikvarma@Karthiks-MacBook-Pro.local>
2022-06-15 03:00:01 -0400

    First bad version

--- Q43.java
+++ Q43.java
@@ -16,6 +16,21 @@ public class Q43 {
         return l;
     }
 
+    // Version 2
+    public int firstBadVersion2(int n) {
+        int left = 1;
+        int right = n;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
     private boolean isBadVersion(int i) {
         return false;
     }
